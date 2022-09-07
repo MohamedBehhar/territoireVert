100adbd05c60c7c908d2deab3c21286ddb3dfe8f
Mohamed Behhar <mbehhar@e3r5p1.1337.ma>
2022-09-07 16:31:58 +0100

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,13 @@ const Footer = () => {
 		<div className='footer'>
 			<div className='footer-center'>
 				<img className='footer-logo' src="images/logo.png" alt="logo-img" />
-				<h4 className='slogan'>Facilitateur de votre transition énergétique</h4>
+				<p className='slogan'>Facilitateur de votre transition énergétique</p>
 				<div className="social-link">
-					<h5>Suivez nous</h5>
 					<a href="https://www.linkedin.com/showcase/territoire-vert" target="_blank" rel="noreferrer" a> <BsLinkedin /></a>
 				</div>
 				<div className="links">
-					<Link  className="link" to="/mention-legales">Mentions légales</Link>
-					<Link className="link" to="/donnees-personnelles" >Politique de confidentialité</Link>
+					<Link  className="link" to="/mention-legales" >Mentions légales</Link> |
+					<Link className="link" to="/donnees-personnelles">Politique de confidentialité</Link>
 				</div>
 				<p>@Territoire Vert-2022</p>
 			</div>
